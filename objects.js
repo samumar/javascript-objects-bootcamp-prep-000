b06ae4b332e68df39cbe38ce58e2dac4525b2301
samumar43 <samumar43@gmail.com>
2020-07-10 23:21:21 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { Slowdive: 'value'};
+
+function updatePlaylist(playlist, artistName, songTitle ) {
+playlist['artistName'] = 'songTitle';
+return playlist;
+}
+
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
+updatePlaylist(playlist, 'Slowdive', "Alison");
+updatePlaylist(playlist, 'My Bloody Valentine', "Sometimes");
